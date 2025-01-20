b920eda7460e1ba224d95d41076b7d1b84ae3704
aykhumajhb024 <aykhumajhb024@student.wethinkcode.co.za>
2025-01-20 10:11:41 +0200

    Start of the fame setting/environment using turtle

---
+++ game.py
@@ -0,0 +1,20 @@
+import turtle
+import time
+import random
+
+#Declare the scores and delay
+delay = 0.3
+score = 0
+
+#Creating the game setting
+game = turtle.Screen()
+game.title("THEEE Snake Game")
+game.bgcolor("aqua")
+game.setup(width = 100, height = 100)
+game.tracer(0)
+
+#The snakes head
+
+
+turtle.done()
+
