7e4463d48e7b95aa9600c0b12b741592cfaf4b81
aykhumajhb024 <aykhumajhb024@student.wethinkcode.co.za>
2025-01-20 10:31:55 +0200

    snake game code continued

--- game.py
+++ game.py
@@ -14,7 +14,28 @@ game.setup(width = 100, height = 100)
 game.tracer(0)
 
 #The snakes head
+head = turtle.Turtle()
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
+#food for the snake
+food = turtle.Turtle()
+food.color("red")
+food.shape("circle")
+food.speed(0)
+food.penup()
+food.goto(0,100)
 
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("black")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,250)
+pen.write("Score : 0 High Score : 0", align = "center", font = ("Comic Sans MS", 24, "bold"))
 turtle.done()
 
